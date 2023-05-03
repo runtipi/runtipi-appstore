570dbb9a24a7357dfaf10017f178ff60b40a5bbc
DrMxrcy <58747968+DrMxrcy@users.noreply.github.com>
2023-05-02 20:34:59 -0400

    Pass Variables via a JS Config File (#446)
    * Pass Variables via a JS Config File
    
    * Create renovate.js

---
+++ renovate.js
@@ -0,0 +1,30 @@
+module.exports = {
+  "$schema": "https://docs.renovatebot.com/renovate-schema.json",
+  "extends": [
+    "config:base"
+  ],
+  "gitIgnoredAuthors": [
+    "githubaction@githubaction.com"
+  ],
+  "dependencyDashboard": true,
+  "enabledManagers": ["docker-compose", "dockerfile"],
+  "hostRules": [
+    {
+      "matchHost": "index.docker.io",
+      "hostType": "docker",
+      "username": process.env.DOCKERHUB_USERNAME,
+      "password": process.env.DOCKERHUB_TOKEN
+    },
+    {
+      "matchHost": "docker.io",
+      "concurrentRequestLimit": 2
+    }
+  ],
+  "packageRules": [
+    {
+      "managers": ["docker-compose", "dockerfile"],
+      "packagePatterns": ["^([^\\/]+\\/)?(mysql|mariadb|mongodb|mongo|postgres|redis)(:|$)"],
+      "enabled": false
+    }
+  ]
+};
