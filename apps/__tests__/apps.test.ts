d79dac6d7a00610d83b4420a4d3961117d1b0212
Nicolas Meienberger <github@thisprops.com>
2023-06-20 21:46:11 +0200

    fix: app version mismatchs between config and docker-compose

--- apps/__tests__/apps.test.ts
+++ apps/__tests__/apps.test.ts
@@ -1,5 +1,6 @@
-import fs from 'fs';
-import jsyaml from 'js-yaml';
+import fs from "fs";
+import semver from "semver";
+import jsyaml from "js-yaml";
 
 interface AppConfig {
   id: string;
@@ -18,11 +19,18 @@ interface AppConfig {
   available: boolean;
 }
 
-const networkExceptions = ['pihole', 'tailscale', 'homeassistant', 'plex', 'zerotier', 'gladys'];
+const networkExceptions = [
+  "pihole",
+  "tailscale",
+  "homeassistant",
+  "plex",
+  "zerotier",
+  "gladys",
+];
 const getAppConfigs = (): AppConfig[] => {
   const apps: AppConfig[] = [];
 
-  const appsDir = fs.readdirSync('./apps');
+  const appsDir = fs.readdirSync("./apps");
 
   appsDir.forEach((app: string) => {
     const path = `./apps/${app}/config.json`;
@@ -36,7 +44,7 @@ const getAppConfigs = (): AppConfig[] => {
           apps.push(config);
         }
       } catch (e) {
-        console.error('Error parsing config file', app);
+        console.error("Error parsing config file", app);
       }
     }
   });
@@ -44,152 +52,198 @@ const getAppConfigs = (): AppConfig[] => {
   return apps;
 };
 
-describe('App configs', () => {
-  it('Get app config should return at least one app', () => {
+describe("App configs", () => {
+  it("Get app config should return at least one app", () => {
     const apps = getAppConfigs();
 
     expect(apps.length).toBeGreaterThan(0);
   });
 
-  it('Each app should have an id', () => {
+  describe("Each app should have an id", () => {
     const apps = getAppConfigs();
 
     apps.forEach((app) => {
-      expect(app.id).toBeDefined();
+      test(app.id, () => {
+        expect(app.id).toBeDefined();
+      });
     });
   });
 
-  it('Each app should have a md description', () => {
+  describe("Each app should have a md description", () => {
     const apps = getAppConfigs();
 
     apps.forEach((app) => {
-      const path = `./apps/${app.id}/metadata/description.md`;
+      test(app.id, () => {
+        const path = `./apps/${app.id}/metadata/description.md`;
 
-      if (fs.existsSync(path)) {
-        const description = fs.readFileSync(path).toString();
-        expect(description).toBeDefined();
-      } else {
-        console.error(`Missing description for app ${app.id}`);
-        expect(true).toBe(false);
-      }
+        if (fs.existsSync(path)) {
+          const description = fs.readFileSync(path).toString();
+          expect(description).toBeDefined();
+        } else {
+          expect(true).toBe(false);
+        }
+      });
     });
   });
 
-  it('Each app should have categories defined as an array', () => {
+  describe("Each app should have categories defined as an array", () => {
     const apps = getAppConfigs();
 
     apps.forEach((app) => {
-      expect(app.categories).toBeDefined();
-      expect(app.categories).toBeInstanceOf(Array);
+      test(app.id, () => {
+        expect(app.categories).toBeDefined();
+        expect(app.categories).toBeInstanceOf(Array);
+      });
     });
   });
 
-  it('Each app should have a name', () => {
+  describe("Each app should have a name", () => {
     const apps = getAppConfigs();
 
     apps.forEach((app) => {
-      expect(app.name).toBeDefined();
+      test(app.id, () => {
+        expect(app.name).toBeDefined();
+      });
     });
   });
 
-  it('Each app should have a description', () => {
+  describe("Each app should have a description", () => {
     const apps = getAppConfigs();
 
     apps.forEach((app) => {
-      expect(app.description).toBeDefined();
+      test(app.id, () => {
+        expect(app.description).toBeDefined();
+      });
     });
   });
 
-  it('Each app should have a port', () => {
+  describe("Each app should have a port", () => {
     const apps = getAppConfigs();
 
     apps.forEach((app) => {
-      expect(app.port).toBeDefined();
-      expect(app.port).toBeGreaterThan(999);
-      expect(app.port).toBeLessThan(65535);
+      test(app.id, () => {
+        expect(app.port).toBeDefined();
+        expect(app.port).toBeGreaterThan(999);
+        expect(app.port).toBeLessThan(65535);
+      });
     });
   });
 
-  it('Each app should have a different port', () => {
+  test("Each app should have a different port", () => {
     const appConfigs = getAppConfigs();
     const ports = appConfigs.map((app) => app.port);
     expect(new Set(ports).size).toBe(appConfigs.length);
   });
 
-  it('Each app should have a unique id', () => {
+  test("Each app should have a unique id", () => {
     const appConfigs = getAppConfigs();
     const ids = appConfigs.map((app) => app.id);
     expect(new Set(ids).size).toBe(appConfigs.length);
   });
 
-  it('Each app should have a version', () => {
+  describe("Each app should have a version", () => {
     const apps = getAppConfigs();
 
     apps.forEach((app) => {
-      expect(app.tipi_version).toBeDefined();
-      expect(app.tipi_version).toBeGreaterThan(0);
+      test(app.id, () => {
+        expect(app.version).toBeDefined();
+        expect(app.tipi_version).toBeDefined();
+        expect(app.tipi_version).toBeGreaterThan(0);
+      });
     });
   });
 
-  it('Each app should have a docker-compose file beside it', () => {
+  describe("Each app should have a docker-compose file beside it", () => {
     const apps = getAppConfigs();
 
     apps.forEach((app) => {
-      expect(fs.existsSync(`./apps/${app.id}/docker-compose.yml`)).toBe(true);
+      test(app.id, () => {
+        expect(fs.existsSync(`./apps/${app.id}/docker-compose.yml`)).toBe(true);
+      });
     });
   });
 
-  it('Each app should have a metadata folder beside it', () => {
+  describe("Each app should have a metadata folder beside it", () => {
     const apps = getAppConfigs();
 
     apps.forEach((app) => {
-      expect(fs.existsSync(`./apps/${app.id}/metadata`)).toBe(true);
+      test(app.id, () => {
+        expect(fs.existsSync(`./apps/${app.id}/metadata`)).toBe(true);
+      });
     });
   });
 
-  it('Each app should have a file named logo.jpg in the metadata folder', () => {
+  describe("Each app should have a file named logo.jpg in the metadata folder", () => {
     const apps = getAppConfigs();
 
     apps.forEach((app) => {
-      expect(fs.existsSync(`./apps/${app.id}/metadata/logo.jpg`)).toBe(true);
+      test(app.id, () => {
+        expect(fs.existsSync(`./apps/${app.id}/metadata/logo.jpg`)).toBe(true);
+      });
     });
   });
 
-  it('Each app should have a container name equals to its id', () => {
+  describe("Each app should have a container name equals to its id", () => {
     const apps = getAppConfigs();
 
     apps.forEach((app) => {
-      const dockerComposeFile = fs.readFileSync(`./apps/${app.id}/docker-compose.yml`).toString();
-
-      const dockerCompose: any = jsyaml.load(dockerComposeFile);
-
-      if (!dockerCompose.services[app.id]) {
-        console.error(app.id);
-      }
-
-      expect(dockerCompose.services[app.id]).toBeDefined();
-      expect(dockerCompose.services[app.id].container_name).toBe(app.id);
-    });
-  });
-
-  it('Each app should have network tipi_main_network', () => {
-    const apps = getAppConfigs();
-
-    apps.forEach((app) => {
-      if (!networkExceptions.includes(app.id)) {
-        const dockerComposeFile = fs.readFileSync(`./apps/${app.id}/docker-compose.yml`).toString();
+      test(app.id, () => {
+        const dockerComposeFile = fs
+          .readFileSync(`./apps/${app.id}/docker-compose.yml`)
+          .toString();
 
         const dockerCompose: any = jsyaml.load(dockerComposeFile);
 
         expect(dockerCompose.services[app.id]).toBeDefined();
+        expect(dockerCompose.services[app.id].container_name).toBe(app.id);
+      });
+    });
+  });
 
-        if (!dockerCompose.services[app.id].networks) {
-          console.error(app.id);
+  describe("Each app should have the same version in config.json and docker-compose.yml", () => {
+    const exceptions = ["revolt"];
+    const apps = getAppConfigs().filter((app) => !exceptions.includes(app.id));
+
+    apps.forEach((app) => {
+      test(app.id, () => {
+        const dockerComposeFile = fs
+          .readFileSync(`./apps/${app.id}/docker-compose.yml`)
+          .toString();
+
+        const dockerCompose: any = jsyaml.load(dockerComposeFile);
+
+        expect(dockerCompose.services[app.id]).toBeDefined();
+        expect(dockerCompose.services[app.id].image).toBeDefined();
+
+        const dockerImage = dockerCompose.services[app.id].image;
+
+        const version = dockerImage.split(":")[1];
+
+        expect(version).toContain(app.version);
+      });
+    });
+  });
+
+  describe("Each app should have network tipi_main_network", () => {
+    const apps = getAppConfigs();
+
+    apps.forEach((app) => {
+      test(app.id, () => {
+        if (!networkExceptions.includes(app.id)) {
+          const dockerComposeFile = fs
+            .readFileSync(`./apps/${app.id}/docker-compose.yml`)
+            .toString();
+
+          const dockerCompose: any = jsyaml.load(dockerComposeFile);
+
+          expect(dockerCompose.services[app.id]).toBeDefined();
+
+          expect(dockerCompose.services[app.id].networks).toBeDefined();
+          expect(dockerCompose.services[app.id].networks).toStrictEqual([
+            "tipi_main_network",
+          ]);
         }
-
-        expect(dockerCompose.services[app.id].networks).toBeDefined();
-        expect(dockerCompose.services[app.id].networks).toStrictEqual(['tipi_main_network']);
-      }
+      });
     });
   });
 });
