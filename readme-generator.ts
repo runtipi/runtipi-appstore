7722d0c86c891c7e49f06eae4617d9faee3defc0
Stavros <steveiliop56@gmail.com>
2024-01-21 19:07:40 +0200

    refactor(readme-generator): use better names (#2261)

--- readme-generator.ts
+++ readme-generator.ts
@@ -45,15 +45,15 @@ const writeToReadme = async (appsList) => {
   let finalReadme = `# Tipi App Store ⛺️
 
 This is the official repository for the Tipi App Store. It contains all the apps that are available for download on [Tipi](https://github.com/meienberger/runtipi).
-    
+
 ## Apps available
-    
+
 ${appsList}
-    
+
 ## How to sumbit an app
-    
+
 If you want to see new apps on Tipi you can either:
-    
+
 - [Open an issue](https://github.com/meienberger/runtipi-appstore/issues) on this repository and members of the community will add it
 - [Join the Discord](https://discord.gg/Bu9qEPnHsc) members of the community will add it.
 - Fork this repo and create the necessary files for a Tipi app. Follow this [guide](<[https://github.com/meienberger/runtipi/wiki/Adding-your-own-app](https://www.runtipi.io/docs/contributing/adding-a-new-app)>)`;
