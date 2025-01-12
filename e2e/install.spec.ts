b5cae96cb3ace5bbbe41c35cabe4218b8861e3f3
Nicolas Meienberger <github@thisprops.com>
2025-01-12 13:39:33 +0100

    chore(cloudflare-ddns): remove port

--- e2e/install.spec.ts
+++ e2e/install.spec.ts
@@ -1,7 +1,7 @@
-import { test, expect } from '@playwright/test';
-import { loginUser } from './fixtures/fixtures';
+import { expect, test } from "@playwright/test";
+import { loginUser } from "./fixtures/fixtures";
 
-test('app should install', async ({ page }) => {
+test("app should install", async ({ page }) => {
   // Set timeout
   test.setTimeout(70000);
 
@@ -9,33 +9,33 @@ test('app should install', async ({ page }) => {
   await loginUser(page);
 
   // Get app from env and go to the app page
-  await page.goto(`http://127.0.0.1/apps/${process.env.APP_NAME || ''}`);
+  await page.goto(`http://127.0.0.1/apps/${process.env.APP_NAME || ""}`);
 
   // Click install button
-  await page.getByRole('button', { name: 'Install' }).click();
+  await page.getByRole("button", { name: "Install" }).click();
 
   // Click install button in modal
-  await page.getByRole('button', { name: 'Install' }).click();
+  await page.getByRole("button", { name: "Install" }).click();
 
   // App should install
-  await expect(page.getByText('Running')).toBeVisible({ timeout: 65000 });
+  await expect(page.getByText("Running")).toBeVisible({ timeout: 65000 });
 });
 
-test('app should...work', async ({ page }) => {
+test("app should...work", async ({ page }) => {
   // Sign in
   await loginUser(page);
 
   // Get app from env and go to the app page
-  await page.goto(`http://127.0.0.1/apps/${process.env.APP_NAME || ''}`);
+  await page.goto(`http://127.0.0.1/apps/${process.env.APP_NAME || ""}`);
 
   // Click open button
-  await page.getByRole('button', { name: 'Open' }).click();
+  await page.getByRole("button", { name: "Open" }).click();
 
   // Open the app
-  await page.getByRole('menuitem', { name: /127.0.0.1/i }).click();
+  await page.getByRole("menuitem", { name: /127.0.0.1/i }).click();
 
   // Wait for page redirect
-  await expect(page.title()).not.toBe(new RegExp('/' + process.env.APP_NAME + '/i'));
+  expect(page.title()).not.toBe(new RegExp(`/${process.env.APP_NAME}/i`));
 
   // Take screenshot
   await page.screenshot({ fullPage: true });
