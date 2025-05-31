d7b6463955b593cc755fd2a62cb4a2e19ae5c5ab
Nicolas Meienberger <github@thisprops.com>
2025-05-31 12:57:30 +0200

    ci(renovate): authenticate dockerhub

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
-export default {
+module.exports = {
   allowedCommands: ["bun ./scripts/update-config.ts", "bun install && bun run test"],
+  secrets: {
+    DOCKERHUB_USERNAME: process.env.DOCKERHUB_USERNAME,
+    DOCKERHUB_TOKEN: process.env.DOCKERHUB_TOKEN,
+  },
 };
 
