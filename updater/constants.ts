577fdb081d7e45c51af6f7135dbbfa1860d9509c
Nicolas Meienberger <github@thisprops.com>
2025-06-01 09:24:42 +0200

    chore: cleanup repo

--- updater/constants.ts
+++
@@ -1,4 +0,0 @@
-import path from "node:path";
-
-export const APPS_DIR = path.join(process.cwd(), "apps");
-export const IGNORED_PREFIXES: string[] = ["postgres", "redis", "mongo", "mysql", "mariadb", "postgres", "immich", "lscr.io"];
