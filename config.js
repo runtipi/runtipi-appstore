8c0977427883fdd44ef4dd01007302074bbd8aac
Nicolas Meienberger <github@thisprops.com>
2025-06-01 09:09:39 +0200

    ci(renovate): move to json

--- config.js
+++ config.js
@@ -1,8 +1,7 @@
 module.exports = {
-  allowedCommands: ["bun ./scripts/update-config.ts", "bun install && bun run test"],
+  allowedCommands: ["bun ./scripts/update-config.ts", "bunx @biomejs/biome check --write", "bun install && bun run test"],
   secrets: {
     DOCKERHUB_USERNAME: process.env.DOCKERHUB_USERNAME,
     DOCKERHUB_TOKEN: process.env.DOCKERHUB_TOKEN,
   },
 };
-
