9d16fa288ebca7ffef18aa7cb34c6c6d0c083f1f
Nicolas Meienberger <github@thisprops.com>
2025-06-02 20:37:11 +0200

    ci(renovate): use host rules in global js config

--- config.js
+++ config.js
@@ -1,7 +1,10 @@
 module.exports = {
   allowedCommands: ["bun ./scripts/update-config.ts", "bunx @biomejs/biome check --write", "bun install && bun run test"],
-  secrets: {
-    DOCKERHUB_USERNAME: process.env.DOCKERHUB_USERNAME,
-    DOCKERHUB_TOKEN: process.env.DOCKERHUB_TOKEN,
-  },
+  hostRules: [
+    {
+      hostType: "docker",
+      username: process.env.DOCKERHUB_USERNAME,
+      password: process.env.DOCKERHUB_TOKEN,
+    },
+  ],
 };
