aa1c2868e061fb0998cad15b0e1d2b93e8453f5a
Nicolas Meienberger <github@thisprops.com>
2025-06-02 21:19:47 +0200

    ci: renovate: split configs

--- config.js
+++ config.js
@@ -1,42 +1,16 @@
 export default {
   allowedCommands: ["bun ./scripts/update-config.ts", "bunx @biomejs/biome check --write", "bun install && bun run test"],
-  automerge: false,
-  rebaseWhen: "conflicted",
-  customManagers: [
-    {
-      customType: "regex",
-      managerFilePatterns: ["^.*docker-compose\\.json$"],
-      matchStrings: ['"image": "(?<depName>.*?):(?<currentValue>.*?)",'],
-      datasourceTemplate: "docker",
-    },
-  ],
-  packageRules: [
-    {
-      matchUpdateTypes: ["major"],
-      addLabels: ["major"],
-    },
-    // {
-    //   "matchUpdateTypes": ["minor"],
-    //   "addLabels": ["minor", "automerge"]
-    // },
-    // {
-    //   "matchUpdateTypes": ["patch"],
-    //   "addLabels": ["patch", "automerge"]
-    // },
-    {
-      matchPackageNames: ["mariadb", "mysql", "monogdb", "postgres", "redis", "immich", "immich{-,}**"],
-      enabled: false,
-    },
-  ],
-  postUpgradeTasks: {
-    commands: ["bun ./scripts/update-config.ts {{packageFile}} {{newVersion}}", "bunx @biomejs/biome check --write", "bun install && bun run test"],
-    fileFilters: ["**/*"],
-    executionMode: "update",
-  },
+
   hostRules: [
     {
       hostType: "docker",
       matchHost: "index.docker.io",
+      username: process.env.DOCKERHUB_USERNAME,
+      password: process.env.DOCKERHUB_TOKEN,
+    },
+    {
+      hostType: "docker",
+      matchHost: "hub.docker.com",
       username: process.env.DOCKERHUB_USERNAME,
       password: process.env.DOCKERHUB_TOKEN,
     },
